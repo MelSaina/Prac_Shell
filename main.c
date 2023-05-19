86610329c2d894e0f5ee0c99b9ca3647119ebee4
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-05-19 14:29:43 -0700

    part 2

--- main.c
+++ main.c
@@ -3,39 +3,75 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include "source.h"
+#include "parser.h"
+#include "executor.h"
 
 int main(int argc, char **argv)
 {
-    char *cmd;
+	char *cmd;
 
-    do
-    {
-        print_prompt1();
+	do
+	{
+		print_prompt1();
 
-        cmd = read_cmd();
+		cmd = read_cmd();
 
-        if(!cmd)
-        {
-            exit(EXIT_SUCCESS);
-        }
+		if(!cmd)
+		{
+			exit(EXIT_SUCCESS);
+		}
 
-        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
-        {
-            free(cmd);
-            continue;
-        }
+		if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+		{
+			free(cmd);
+			continue;
+		}
 
-        if(strcmp(cmd, "exit\n") == 0)
-        {
-            free(cmd);
-            break;
-        }
+		if(strcmp(cmd, "exit\n") == 0)
+		{
+			free(cmd);
+			break;
+		}
 
-        printf("%s\n", cmd);
+		struct source_s src;
+		src.buffer   = cmd;
+		src.bufsize  = strlen(cmd);
+		src.curpos   = INIT_SRC_POS;
+		parse_and_execute(&src);
 
-        free(cmd);
+		free(cmd);
 
-    } while(1);
+		} while(1);
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
+}
+
+
+int parse_and_execute(struct source_s *src)
+{
+	skip_white_spaces(src);
+
+	struct token_s *tok = tokenize(src);
+
+	if(tok == &eof_token)
+	{
+		return 0;
+	}
+
+	while(tok && tok != &eof_token)
+	{
+		struct node_s *cmd = parse_simple_command(tok);
+
+        	if(!cmd)
+		{
+			break;
+		}
+
+		do_simple_command(cmd);
+		free_node_tree(cmd);
+		tok = tokenize(src);
+	}
+
+	return 1;
 }
--- shell.h
+++ shell.h
@@ -1,5 +1,8 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include "source.h"
+
+int  parse_and_execute(struct source_s *src);
 void print_prompt1(void);
 char *read_cmd(void);
 #endif
